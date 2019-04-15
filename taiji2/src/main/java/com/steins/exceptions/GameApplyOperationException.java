fc8dc66d6d95cc5607acccbabca991ccccb7b56b
GuaGod <605564874@qq.com>
2019-04-15 16:01:31 +0800

    删除尹国庆错误的上传

--- taiji2/src/main/java/com/steins/exceptions/GameApplyOperationException.java
+++
@@ -1,9 +0,0 @@
-package com.steins.exceptions;
-
-public class GameApplyOperationException extends RuntimeException {
-	private static final long serialVersionUID = -1512771573934050550L;
-	
-	public GameApplyOperationException(String msg) {
-		super(msg);
-	}
-}
